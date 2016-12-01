824c5027926d12938119139eeea35fc5951ea0d1
forrana <forrana@gmail.com>
2016-12-02 01:35:20 +0300

    Optimizing acceleration physics, refactor Player class

--- src/scripts/world/Player.ts
+++ src/scripts/world/Player.ts
@@ -1,4 +1,177 @@
 module Gravity {
+    export class Player extends Phaser.Sprite {
+
+        map : Gravity.Map;
+        weapon;
+        keyScheme;
+        keyScheme1;
+        keyScheme2;
+        bullets;
+        hitPoints;
+        alive;
+        currentThrust;
+        maxThrust;
+        playerCollisionGroup;
+        weaponCollisionGroup;
+
+        constructor(game: Phaser.Game,
+                    x: number,
+                    y: number,
+                    map: Gravity.Map,
+                    model: string,
+                    keyScheme: string
+                    ) {
+
+            super(game, x, y, model);
+            this.currentThrust = 20;
+            this.maxThrust = 500;
+
+            this.keyScheme = keyScheme;
+            // this.weapon = new RGBLaster(this.game, 40);
+            this.scale.setTo(0.3, 0.3);
+            this.map = map;
+            this.anchor.set(0.5);
+
+            this.hitPoints = {current: 20, max: 20};
+            this.alive = true;
+
+            this.weapon = this.game.add.weapon(40, 'bullet');
+            this.weapon.offsetX = 0;
+            this.weapon.offsetY = 0;
+            this.weapon.setBulletFrames(0, 80, true);
+            this.weapon.bulletAngleOffset = -90;
+            this.weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
+            //  The speed at which the bullet is fired
+            this.weapon.bulletSpeed = 400;
+            //  Speed-up the rate of fire, allowing them to shoot 1 bullet every 50ms
+            this.weapon.fireRate = 10;
+            this.weapon.trackSprite(this, 0, 0, false);
+            this.weapon.bulletKillDistance = 100;
+            this.weapon.bulletRotateToVelocity = true;
+            // this.weapon.bulletInheritSpriteSpeed = true;
+
+            this.bullets = game.add.group();
+            this.bullets.enableBody = true;
+            this.bullets.physicsBodyType = Phaser.Physics.P2JS;
+            this.bullets.createMultiple(1000, 'bullet', 0, false);
+            this.bullets.setAll('anchor.x', 0);
+            this.bullets.setAll('anchor.y', 0);
+
+            this.weapon.bullets = this.bullets;
+            //  The speed at which the bullet is fired
+
+            let cursors = game.input.keyboard.createCursorKeys(),
+                fireButton = this.game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR),
+                fireButton1 = this.game.input.keyboard.addKey(Phaser.KeyCode.SHIFT),
+                leftKey = this.game.input.keyboard.addKey(Phaser.KeyCode.A),
+                rightKey = this.game.input.keyboard.addKey(Phaser.KeyCode.D),
+                forward = this.game.input.keyboard.addKey(Phaser.KeyCode.W),
+                backward = this.game.input.keyboard.addKey(Phaser.KeyCode.S);
+
+            this.keyScheme1 = {
+                left: cursors.left,
+                right: cursors.right,
+                forward: cursors.up,
+                backward: cursors.down,
+                shoot: fireButton
+            }
+
+            this.keyScheme2 = {
+                left: leftKey,
+                right: rightKey,
+                forward: forward,
+                backward: backward,
+                shoot: fireButton1
+            }
+
+            game.add.existing(this);
+        }
+
+        setCollisionGroups (playerCollisionGroup, weaponCollisionGroup) {
+            this.playerCollisionGroup = playerCollisionGroup;
+            this.weaponCollisionGroup = weaponCollisionGroup;
+            this.bullets.setAll('setCollisionGroup', this.weaponCollisionGroup);
+        }
+
+        screenWrap (sprite) {
+            if (sprite.x < 0)
+            {
+                sprite.x = this.game.width;
+            }
+            else if (sprite.x > this.game.width)
+            {
+                sprite.x = 0;
+            }
+
+            if (sprite.y < 0)
+            {
+                sprite.y = this.game.height;
+            }
+            else if (sprite.y > this.game.height)
+            {
+                sprite.y = 0;
+            }
+        }
+
+        shoot() {
+            this.body.reverse(200);
+            this.weapon.fireAngle = this.body.angle - 90;
+            this.weapon.fire();
+        }
+
+        keyControlls(keyScheme) {
+            this.currentThrust > 0 ?
+                this.body.thrust(this.currentThrust) :
+                this.body.reverse(Math.abs(this.currentThrust));
+
+            if (keyScheme.left.isDown) {
+                this.body.rotateLeft(100);
+            }
+            else if (keyScheme.right.isDown) {
+                this.body.rotateRight(100);
+            }
+            else {
+                this.body.setZeroRotation();
+            }
+
+            if (keyScheme.forward.isDown) {
+                this.currentThrust < this.maxThrust ?
+                    this.currentThrust+=1 : this.currentThrust += 0;
+                    console.log(this.currentThrust);
+            }
+            else if (keyScheme.backward.isDown) {
+                this.currentThrust > this.maxThrust*-1 ?
+                    this.currentThrust-=1 : this.currentThrust += 0;
+                    console.log(this.currentThrust);
+            }
+
+            if (keyScheme.shoot.isDown)
+            {
+                this.shoot();
+            }
+        }
+
+        update() {
+            switch(this.keyScheme){
+                case 'keyScheme1': this.keyControlls(this.keyScheme1); break;
+                case 'keyScheme2': this.keyControlls(this.keyScheme2); break;
+                default: this.keyControlls(this.keyScheme1); break;
+            }
+            this.game.world.wrap(this, 16);
+        }
+
+        damage() {
+            console.warn('Hit!!!', this.hitPoints);
+            if(this.hitPoints.current > 0) {
+                this.hitPoints.current -= 1;
+            } else {
+                this.hitPoints.current = this.hitPoints.max;
+                this.reset(this.game.rnd.integerInRange(20, 1000), this.game.rnd.integerInRange(20, 480));
+            }
+            return false;
+        }
+    }
+
     Phaser.Weapon.prototype.fire = function (from, x, y) {
 
         if (this.game.time.now < this._nextFire || (this.fireLimit > 0 && this.shots === this.fireLimit))
@@ -206,212 +379,4 @@ module Gravity {
         }
         return bullet;
     };
-
-
-    export class Player extends Phaser.Sprite {
-
-        cursors;
-        fireButton;
-        map : Gravity.Map;
-        weapon;
-        fireButton1;
-        leftKey;
-        rightKey;
-        forward;
-        backward;
-        keyScheme;
-        playerCollisionGroup;
-        weaponCollisionGroup;
-        bullets;
-        hitPoints;
-        alive;
-        currentThrust;
-        maxThrust;
-
-        constructor(game: Phaser.Game,
-                    x: number,
-                    y: number,
-                    map: Gravity.Map,
-                    model: string,
-                    keyScheme: string
-                    ) {
-
-            super(game, x, y, model);
-            this.currentThrust = 20;
-            this.maxThrust = 500;
-
-            this.keyScheme = keyScheme;
-            // this.weapon = new RGBLaster(this.game, 40);
-            this.scale.setTo(0.3, 0.3);
-            this.map = map;
-            this.anchor.set(0.5);
-
-        //    this.health = new Phaser.Component.Health();
-            this.hitPoints = {current: 20, max: 20};
-            // this.health.alive = true;
-            // this.health.kill = () => {
-            //     this.health = new Phaser.Component.Health();
-            //     this.health.setHealth(49);
-            //     this.health.alive = true;
-            //
-            //     this.reset(game.rnd.integerInRange(20, 1000), game.rnd.integerInRange(20, 480));
-            //
-            //     return true;
-            // }
-            this.alive = true;
-
-            this.weapon = this.game.add.weapon(40, 'bullet');
-            this.weapon.offsetX = 0;
-            this.weapon.offsetY = 0;
-            this.weapon.setBulletFrames(0, 80, true);
-            this.weapon.bulletAngleOffset = -90;
-            this.weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
-            //  The speed at which the bullet is fired
-            this.weapon.bulletSpeed = 400;
-            //  Speed-up the rate of fire, allowing them to shoot 1 bullet every 50ms
-            this.weapon.fireRate = 10;
-            this.weapon.trackSprite(this, 0, 0, false);
-            this.weapon.bulletKillDistance = 100;
-            this.weapon.bulletRotateToVelocity = true;
-            this.weapon.bulletInheritSpriteSpeed
-            // this.weapon.bulletInheritSpriteSpeed = true;
-
-            this.bullets = game.add.group();
-            this.bullets.enableBody = true;
-            this.bullets.physicsBodyType = Phaser.Physics.P2JS;
-            this.bullets.createMultiple(1000, 'bullet', 0, false);
-            this.bullets.setAll('anchor.x', 0);
-            this.bullets.setAll('anchor.y', 0);
-
-            this.weapon.bullets = this.bullets;
-            // this.bullets.setAll('body.fixedRotation', true);
-            //  The speed at which the bullet is fired
-
-            this.cursors = game.input.keyboard.createCursorKeys();
-            this.fireButton = this.game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
-            this.fireButton1 = this.game.input.keyboard.addKey(Phaser.KeyCode.SHIFT);
-            this.leftKey = this.game.input.keyboard.addKey(Phaser.KeyCode.A);
-            this.rightKey = this.game.input.keyboard.addKey(Phaser.KeyCode.D);
-            this.forward = this.game.input.keyboard.addKey(Phaser.KeyCode.W);
-            this.backward = this.game.input.keyboard.addKey(Phaser.KeyCode.S);
-
-            game.add.existing(this);
-        }
-
-        setCollisionGroups (playerCollisionGroup, weaponCollisionGroup) {
-            this.playerCollisionGroup = playerCollisionGroup;
-            this.weaponCollisionGroup = weaponCollisionGroup;
-            this.bullets.setAll('setCollisionGroup', this.weaponCollisionGroup);
-        }
-
-        screenWrap (sprite) {
-            if (sprite.x < 0)
-            {
-                sprite.x = this.game.width;
-            }
-            else if (sprite.x > this.game.width)
-            {
-                sprite.x = 0;
-            }
-
-            if (sprite.y < 0)
-            {
-                sprite.y = this.game.height;
-            }
-            else if (sprite.y > this.game.height)
-            {
-                sprite.y = 0;
-            }
-        }
-
-        keyScheme1() {
-            this.currentThrust > 0 ?
-                this.body.thrust(this.currentThrust) :
-                this.body.reverse(Math.abs(this.currentThrust));
-
-            if (this.cursors.left.isDown) {
-                this.body.rotateLeft(100);
-            }
-            else if (this.cursors.right.isDown) {
-                this.body.rotateRight(100);
-            }
-            else {
-                this.body.setZeroRotation();
-            }
-
-            if (this.cursors.up.isDown) {
-                this.currentThrust < this.maxThrust ?
-                    this.currentThrust+=10 : this.currentThrust += 0;
-                    console.log(this.currentThrust);
-            }
-            else if (this.cursors.down.isDown) {
-                this.currentThrust > this.maxThrust*-1 ?
-                    this.currentThrust-=10 : this.currentThrust += 0;
-                    console.log(this.currentThrust);
-            }
-
-            if (this.fireButton.isDown)
-            {
-                this.body.reverse(200);
-                this.weapon.fireAngle = this.body.angle - 90;
-                this.weapon.fire();
-                //null, this.body.x*Math.sin(this.body.angle),
-                // this.body.y*Math.con(this.body.angle)
-            }
-        }
-        keyScheme2(){
-            this.currentThrust > 0 ?
-                this.body.thrust(this.currentThrust) :
-                this.body.reverse(this.currentThrust*-1);
-
-
-            if (this.leftKey.isDown) {
-                this.body.rotateLeft(100);
-            }
-            else if (this.rightKey.isDown) {
-                this.body.rotateRight(100);
-            }
-            else {
-                this.body.setZeroRotation();
-            }
-
-            if (this.forward.isDown) {
-                // this.body.thrust(650);
-                this.currentThrust < this.maxThrust ?
-                    this.currentThrust+=10 : this.currentThrust += 0;
-            }
-            else if (this.backward.isDown) {
-                // this.currentThrust-=10;
-                this.currentThrust < this.maxThrust ?
-                    this.currentThrust-=10 : this.currentThrust += 0;
-            }
-
-            if (this.fireButton1.isDown)
-            {
-                this.body.reverse(100);
-                this.weapon.fireAngle = this.body.angle - 90;
-                this.weapon.fire();
-            }
-        }
-
-        update() {
-            switch(this.keyScheme){
-                case 'keyScheme1': this.keyScheme1(); break;
-                case 'keyScheme2': this.keyScheme2(); break;
-            }
-            this.game.world.wrap(this, 16);
-        }
-
-        damage() {
-            console.warn('Hit!!!', this.hitPoints);
-            if(this.hitPoints.current > 0) {
-                this.hitPoints.current -= 1;
-            } else {
-                this.hitPoints.current = this.hitPoints.max;
-                this.reset(this.game.rnd.integerInRange(20, 1000), this.game.rnd.integerInRange(20, 480));
-            }
-            return false;
-        }
-
-    }
 }
